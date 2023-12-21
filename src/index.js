2be4d35a03b68742d000b04c6c826d99ecd54f5a
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-22 00:01:31 +0100

    Add new style.css file in src folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import myName from './myName';
-
+import './style.css';
 function component() {
     const element = document.createElement('div');
   
     // use the imported myName function
     element.innerText = myName('Ndzi Nfor');
-  
+    element.classList.add('hello');
     return element;
   }
   
