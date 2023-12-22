320e100e8d7881c3cda0e271c338e497caab2e42
Ndzi Nfor <ndzinfor@gmail.com>
2023-12-22 08:34:04 +0100

    import data files in js file within src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import myName from './myName';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
@@ -8,12 +10,18 @@ function component() {
     // use the imported myName function
     element.innerText = myName('Ndzi Nfor');
     element.classList.add('hello');
-    return element;
+    
 
     //add icon img to our existing div.
     const myIcon = new Image();
     myIcon.src = Icon;
+
     element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+
+    return element;
   }
   
   document.body.appendChild(component());
